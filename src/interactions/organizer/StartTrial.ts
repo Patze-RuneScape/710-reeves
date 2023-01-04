f5dcf66d2e53515270f961c18cb3361c7df3f74f
Nex AoD <114638787+nexaod@users.noreply.github.com>
2023-01-04 23:06:19 +0800

    Add `start-trial` command

---
+++ src/interactions/organizer/StartTrial.ts
@@ -0,0 +1,244 @@
+import BotInteraction from '../../types/BotInteraction';
+import { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, Role, TextChannel, User } from 'discord.js';
+
+interface Categories {
+    [category: string]: string[];
+}
+
+interface TrialledRole {
+    key: string;
+    role: Role;
+}
+
+export default class Pass extends BotInteraction {
+    get name() {
+        return 'start-trial';
+    }
+
+    get description() {
+        return 'Starts a Trial';
+    }
+
+    get permissions() {
+        return 'TRIAL_TEAM';
+    }
+
+    get roleOptions() {
+        const assignOptions: any = {
+            'Magic Minion Tank': 'magicMT',
+            'Magic Base': 'magicBase',
+            'Umbra Minion Tank - Magic': 'magicUmbra',
+            'Range Minion Tank': 'rangeMT',
+            'Umbra Minion Tank - Range': 'rangeUmbra',
+            'Chinner': 'chinner',
+            'Magic/Range Minion Tank': 'mrMT',
+            'Magic/Range Hammer': 'mrHammer',
+            'Magic/Range Base': 'mrBase',
+        }
+        const options: any = [];
+        Object.keys(assignOptions).forEach((key: string) => {
+            options.push({ name: key, value: assignOptions[key] })
+        })
+        return options;
+    }
+
+    get regionOptions() {
+        const assignOptions: any = {
+            'North America': 'North America',
+            'Europe': 'German',
+        }
+        const options: any = [];
+        Object.keys(assignOptions).forEach((key: string) => {
+            options.push({ name: key, value: assignOptions[key] })
+        })
+        return options;
+    }
+
+    get trialOptions() {
+        const trialTypes: any = {
+            'Mock': 'mock',
+            'Real': 'real',
+        }
+        const options: any = [];
+        Object.keys(trialTypes).forEach((key: string) => {
+            options.push({ name: key, value: trialTypes[key] })
+        })
+        return options;
+    }
+
+    get categories(): Categories {
+        return {
+            base: ['magicBase', 'mrBase'],
+            umbra: ['magicUmbra', 'rangeUmbra'],
+            cruor: ['magicMT', 'rangeMT', 'mrMT'],
+            hammer: ['chinner', 'mrHammer'],
+        }
+    }
+
+    get slashData() {
+        return new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption((option) => option.setName('user').setDescription('User').setRequired(true))
+            .addStringOption((option) => option.setName('role').setDescription('Trialee preferred role').addChoices(
+                ...this.roleOptions
+            ).setRequired(true))
+            .addStringOption((option) => option.setName('region').setDescription('Trial world').addChoices(
+                ...this.regionOptions
+            ).setRequired(true))
+            .addStringOption((option) => option.setName('type').setDescription('Trial type').addChoices(
+                ...this.trialOptions
+            ).setRequired(true))
+            .addStringOption((option) => option.setName('time').setDescription('Time of trial. Must be in the format YYYY-MM-DD HH:MM in Gametime. e.g. 2022-11-05 06:00').setRequired(false))
+    }
+
+    public getTrialledRole = async (interaction: ChatInputCommandInteraction, roleKey: string): Promise<TrialledRole | undefined> => {
+        const { roles, stripRole } = this.client.util;
+        console.log(roles[roleKey])
+        if (!roles[roleKey]) return;
+        const roleObject = await interaction.guild?.roles.fetch(stripRole(roles[roleKey])) as Role;
+        return {
+            key: roles[roleKey],
+            role: roleObject
+        };
+    }
+
+    public parseTime = (timeString: string): string => {
+        const [date, time] = timeString.split(' ');
+        const [year, month, day] = date.split('-').map(Number);
+        const [hours, minutes] = time.split(':').map(Number);
+        const gametime = new Date(Date.UTC(year, month - 1, day, hours, minutes))
+        return `<t:${Math.round(gametime.getTime() / 1000)}:f>`;
+    }
+
+    async run(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply({ ephemeral: true });
+        const role: string = interaction.options.getString('role', true);
+        const user: User = interaction.options.getUser('user', true);
+        const region: string = interaction.options.getString('region', true);
+        const time: string | null = interaction.options.getString('time', false);
+
+        const { roles, colours, channels, emojis } = this.client.util;
+
+        const expression = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})$/;
+
+        let errorMessage = '';
+
+        if (time) {
+            const isValid = expression.test(time);
+            if (!isValid) {
+                errorMessage += '\n\nThe trial `time` was not in the expected format.'
+            }
+        }
+
+        const errorEmbed = new EmbedBuilder()
+            .setTitle('Something went wrong!')
+            .setColor(colours.discord.red)
+            .setDescription(errorMessage || 'No error message.');
+        if (time) {
+            const isValid = expression.test(time);
+            console.log(isValid)
+            if (!isValid) {
+                return await interaction.editReply({ embeds: [errorEmbed] });
+            }
+        }
+
+        const roleInfo = await this.getTrialledRole(interaction, role);
+        console.log(role)
+        console.log(roleInfo)
+
+        const firstRow = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('selectUmbra')
+                    .setEmoji(emojis.umbra)
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId('selectGlacies')
+                    .setEmoji(emojis.glacies)
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId('selectCruor')
+                    .setEmoji(emojis.cruor)
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId('selectFumus')
+                    .setEmoji(emojis.fumus)
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId('selectHammer')
+                    .setEmoji(emojis.hammer)
+                    .setStyle(ButtonStyle.Secondary),
+            );
+
+        const secondRow = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('selectBase')
+                    .setLabel('Base')
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId('selectFree')
+                    .setLabel('Free')
+                    .setStyle(ButtonStyle.Secondary),
+            );
+
+        const controlPanel = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('startTrial')
+                    .setLabel('Start Trial')
+                    .setStyle(ButtonStyle.Success),
+                new ButtonBuilder()
+                    .setCustomId('disbandTrial')
+                    .setLabel('Disband')
+                    .setStyle(ButtonStyle.Danger)
+            );
+
+        const checkRole = (category: string) => {
+            return this.categories[category].includes(role) ? `<@${user.id}> (Trialee)` : '`Empty`';
+        }
+
+        const fields = [
+            { name: `${emojis.voke} Base`, value: checkRole('base'), inline: true },
+            { name: `${emojis.umbra} Umbra`, value: checkRole('umbra'), inline: true },
+            { name: `${emojis.glacies} Glacies`, value: '`Empty`', inline: true },
+            { name: `${emojis.cruor} Cruor`, value: checkRole('cruor'), inline: true },
+            { name: `${emojis.fumus} Fumus`, value: '`Empty`', inline: true },
+            { name: `${emojis.hammer} Hammer`, value: checkRole('hammer'), inline: true },
+            { name: `${emojis.freedom} Free`, value: '`Empty`', inline: true },
+        ]
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() || this.client.user?.avatarURL() || 'https://media.discordapp.net/attachments/1027186342620299315/1047598720834875422/618px-Solly_pet_1.png' })
+            .setColor(roleInfo?.role.color || colours.lightblue)
+            .setDescription(`
+            > **General**\n
+            \`Host:\` <@${interaction.user.id}>
+            ${time ?
+                    `\`Game Time:\` \`${time}\`
+            \`Local Time:\` ${this.parseTime(time)}`
+                    :
+                    `\`Time:\` \`ASAP\``}
+            \`World:\` ${region}\n
+            > **Trialee**\n
+            \`Discord:\` <@${user.id}>
+            \`Tag:\` ${roleInfo?.key}\n
+            > **Team**
+            `)
+            .addFields(
+                fields
+            );
+
+        const channel = await this.client.channels.fetch(channels.botRoleLog) as TextChannel;
+        await channel.send(
+            { embeds: [embed], components: [firstRow, secondRow, controlPanel] }
+        )
+
+        const replyEmbed = new EmbedBuilder()
+            .setTitle('Trial notification created!')
+            .setColor(colours.discord.green)
+            .setDescription(`${roles['trialTeam']} has been notified in <#${channels.botRoleLog}>`);
+        await interaction.editReply({ embeds: [replyEmbed] });
+    }
+}
